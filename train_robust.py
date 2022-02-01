041095cc57de2542a675f9b98592031d1857d0e9
Sahil Singla <singlasahil14@gmail.com>
2022-02-01 15:29:55 +0530

    Update train_robust.py

--- train_robust.py
+++ train_robust.py
@@ -4,6 +4,7 @@ import logging
 import os
 import time
 import math
+from shutil import copyfile
 
 import numpy as np
 import torch
@@ -11,53 +12,95 @@ import torch.nn as nn
 import torch.nn.functional as F
 from apex import amp
 
-from robust_net import LipNet_n
-from utils import (upper_limit, lower_limit, cifar10_mean, cifar10_std, clamp, get_loaders,
-    attack_pgd, evaluate_pgd, evaluate_standard, evaluate_certificates)
-from utils_conv import test_real_sn
+from lip_convnets import LipConvNet
+from utils import *
 
 logger = logging.getLogger(__name__)
 
 def get_args():
     parser = argparse.ArgumentParser()
+    
+    # Training specifications
     parser.add_argument('--batch-size', default=128, type=int)
-    parser.add_argument('--data-dir', default='./cifar-data', type=str)
     parser.add_argument('--epochs', default=200, type=int)
-    parser.add_argument('--lr-schedule', default='multistep', choices=['cyclic', 'multistep'])
     parser.add_argument('--lr-min', default=0., type=float)
     parser.add_argument('--lr-max', default=0.1, type=float)
     parser.add_argument('--weight-decay', default=5e-4, type=float)
     parser.add_argument('--momentum', default=0.9, type=float)
-    parser.add_argument('--epsilon', default=36, type=int)
-    parser.add_argument('--in-channels', default=16, type=int)
-    parser.add_argument('--block-size', default=4, type=int, help='model type')
-    parser.add_argument('--out-dir', default='lipnet', type=str, help='Output directory')
-    parser.add_argument('--seed', default=0, type=int, help='Random seed')
-    parser.add_argument('--opt-level', default='O2', type=str, choices=['O0', 'O1', 'O2'],
-        help='O0 is FP32 training, O1 is Mixed Precision, and O2 is "Almost FP16" Mixed Precision')
-    parser.add_argument('--conv-type', default='standard', type=str, choices=['standard', 'bcop', 
-        'skew'], help='standard, skew symmetric or bcop convolution')
-    parser.add_argument('--dataset', default='cifar10', type=str, choices=['cifar10', 'cifar100'], 
-                        help='dataset to use for training')
+    parser.add_argument('--gamma', default=0., type=float, help='gamma for certificate regularization')
+    parser.add_argument('--opt-level', default='O2', type=str, choices=['O0', 'O2'],
+        help='O0 is FP32 training and O2 is "Almost FP16" Mixed Precision')
     parser.add_argument('--loss-scale', default='1.0', type=str, choices=['1.0', 'dynamic'],
         help='If loss_scale is "dynamic", adaptively adjust the loss scale over time')
+
+    
+    # Model architecture specifications
+    parser.add_argument('--conv-layer', default='soc', type=str, choices=['bcop', 'cayley', 'soc'], 
+                        help='BCOP, Cayley, SOC convolution')
+    parser.add_argument('--init-channels', default=32, type=int)
+    parser.add_argument('--activation', default='maxmin', choices=['maxmin', 'hh1', 'hh2'], 
+                        help='Activation function')
+    parser.add_argument('--block-size', default=1, type=int, choices=[1, 2, 3, 4, 5, 6, 7, 8], 
+                        help='size of each block')
+    parser.add_argument('--lln', action='store_true', help='set last linear to be linear and normalized')
+
+    
+    # Dataset specifications
+    parser.add_argument('--data-dir', default='./cifar-data', type=str)
+    parser.add_argument('--dataset', default='cifar10', type=str, choices=['cifar10', 'cifar100'], 
+                        help='dataset to use for training')
+    
+    # Other specifications
+    parser.add_argument('--epsilon', default=36, type=int)
+    parser.add_argument('--out-dir', default='test/LipConvnet', type=str, help='Output directory')
+    parser.add_argument('--seed', default=0, type=int, help='Random seed')
     return parser.parse_args()
 
+def init_model(args):
+    model = LipConvNet(args.conv_layer, args.activation, init_channels=args.init_channels, 
+                       block_size = args.block_size, num_classes=args.num_classes, 
+                       lln=args.lln)
+    return model
+
+def robust_statistics(losses_arr, correct_arr, certificates_arr, 
+                      epsilon_list=[36., 72., 108., 144., 180., 216.]):
+    mean_loss = np.mean(losses_arr)
+    mean_acc = np.mean(correct_arr)
+    mean_certs = (certificates_arr * correct_arr).sum()/correct_arr.sum()
+    
+    robust_acc_list = []
+    for epsilon in epsilon_list:
+        robust_correct_arr = (certificates_arr > (epsilon/255.)) & correct_arr
+        robust_acc = robust_correct_arr.sum()/robust_correct_arr.shape[0]
+        robust_acc_list.append(robust_acc)
+    return mean_loss, mean_acc, mean_certs, robust_acc_list
 
 def main():
     args = get_args()
     
-    if args.dataset == 'cifar10':
-        args.in_channels = 32
-    elif args.dataset == 'cifar100':
-        args.in_channels = 32
-    else:
-        raise Exception('Unknown dataset ', args.dataset)
-        
-    args.out_dir = args.out_dir + '_' + str(args.dataset) + '_' + str(args.block_size) + '_' + str(args.conv_type)
-    args.block_size = int(args.block_size)
+    if args.conv_layer == 'cayley' and args.opt_level == 'O2':
+        raise ValueError('O2 optimization level is incompatible with Cayley Convolution')
+
+    args.out_dir += '_' + str(args.dataset) 
+    args.out_dir += '_' + str(args.block_size) 
+    args.out_dir += '_' + str(args.conv_layer)
+    args.out_dir += '_' + str(args.init_channels)
+    args.out_dir += '_' + str(args.activation)
+    args.out_dir += '_cr' + str(args.gamma)
+    if args.lln:
+        args.out_dir += '_lln'
+    
     
     os.makedirs(args.out_dir, exist_ok=True)
+    code_dir = os.path.join(args.out_dir, 'code')
+    os.makedirs(code_dir, exist_ok=True)
+    for f in os.listdir('./'):
+        src = os.path.join('./', f)
+        dst = os.path.join(code_dir, f)
+        if os.path.isfile(src):
+            if f[-3:] == '.py' or f[-3:] == '.sh':
+                copyfile(src, dst)
+    
     logfile = os.path.join(args.out_dir, 'output.log')
     if os.path.exists(logfile):
         os.remove(logfile)
@@ -73,23 +116,24 @@ def main():
     torch.cuda.manual_seed(args.seed)
 
     train_loader, test_loader = get_loaders(args.data_dir, args.batch_size, args.dataset)
-    im_size = 32
     std = cifar10_std
     if args.dataset == 'cifar10':
-        num_classes = 10    
+        args.num_classes = 10    
     elif args.dataset == 'cifar100':
-        num_classes = 100
+        args.num_classes = 100
     else:
         raise Exception('Unknown dataset')
 
     # Evaluation at early stopping
-    model = LipNet_n(args.conv_type, in_channels=args.in_channels, 
-                     num_blocks = args.block_size, num_classes=num_classes).cuda()
+    model = init_model(args).cuda()
     model.train()
-    
-    if args.conv_type == 'skew':
-        opt = torch.optim.SGD(model.parameters(), lr=args.lr_max, momentum=args.momentum, 
-                              weight_decay=args.weight_decay)
+
+    conv_params, activation_params, other_params = parameter_lists(model)
+    if args.conv_layer == 'soc':
+        opt = torch.optim.SGD([
+                        {'params': activation_params, 'weight_decay': 0.},
+                        {'params': (conv_params + other_params), 'weight_decay': args.weight_decay}
+                    ], lr=args.lr_max, momentum=args.momentum)
     else:
         opt = torch.optim.SGD(model.parameters(), lr=args.lr_max, momentum=args.momentum, 
                               weight_decay=0.)
@@ -101,20 +145,20 @@ def main():
     criterion = nn.CrossEntropyLoss()
 
     lr_steps = args.epochs * len(train_loader)
-    if args.lr_schedule == 'cyclic':
-        scheduler = torch.optim.lr_scheduler.CyclicLR(opt, base_lr=args.lr_min, max_lr=args.lr_max,
-            step_size_up=lr_steps / 20, step_size_down= (3 * lr_steps) / 20)
-    elif args.lr_schedule == 'multistep':
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(opt, milestones=[lr_steps // 2, 
-            (3 * lr_steps) // 4], gamma=0.1)
-
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(opt, milestones=[lr_steps // 2, 
+        (3 * lr_steps) // 4], gamma=0.1)
+    
+    best_model_path = os.path.join(args.out_dir, 'best.pth')
+    last_model_path = os.path.join(args.out_dir, 'last.pth')
+    last_opt_path = os.path.join(args.out_dir, 'last_opt.pth')
+    
     # Training
     std = torch.tensor(std).cuda()
     L = 1/torch.max(std)
-    prev_test_acc = 0.
+    prev_robust_acc = 0.
     start_train_time = time.time()
-    logger.info('Epoch \t Seconds \t LR \t Train Loss \t Train Acc \t Train Robust \t ' + 
-                'Train Cert \t Test Loss \t Test Acc \t Test Robust \t Test Cert')
+    logger.info('Epoch \t Seconds \t LR \t Train Loss \t Train Acc \t Test Loss \t ' + 
+                'Test Acc \t Test Robust (36) \t Test Robust (72) \t Test Robust (108) \t Test Cert')
     for epoch in range(args.epochs):
         model.train()
         start_epoch_time = time.time()
@@ -127,25 +171,20 @@ def main():
             X, y = X.cuda(), y.cuda()
             
             output = model(X)
-            output_y = output[torch.arange(X.shape[0]), y]
-            
-            onehot_y = torch.zeros_like(output).cuda()
-            onehot_y[torch.arange(output.shape[0]), y] = 1.
-            output_trunc = output - onehot_y*1e3
-            
-            output_runner_up, _ = torch.max(output_trunc, dim=1)
-            margin = output_y - output_runner_up
+            curr_correct = (output.max(1)[1] == y)
+            if args.lln:
+                curr_cert = lln_certificates(output, y, model.last_layer, L)
+            else:
+                curr_cert = ortho_certificates(output, y, L)
+                
             ce_loss = criterion(output, y)
-
-            curr_cert = margin/(math.sqrt(2)*L)
-            loss = ce_loss
+            loss = ce_loss - args.gamma * F.relu(curr_cert).mean()
 
             opt.zero_grad()
             with amp.scale_loss(loss, opt) as scaled_loss:
                 scaled_loss.backward()
             opt.step()
 
-            curr_correct = (output.max(1)[1] == y)
             train_loss += ce_loss.item() * y.size(0)
             train_cert += (curr_cert * curr_correct).sum().item()
             train_robust += ((curr_cert > (args.epsilon/255.)) * curr_correct).sum().item()
@@ -153,69 +192,69 @@ def main():
             train_n += y.size(0)
             scheduler.step()
             
-        last_state_dict = copy.deepcopy(model.state_dict())
-            
         # Check current test accuracy of model
-        test_loss, test_acc, mean_cert, robust_acc = evaluate_certificates(test_loader, model, L)
-        if (robust_acc > prev_test_acc):
-            model_path = os.path.join(args.out_dir, 'best.pth')
-            best_state_dict = copy.deepcopy(model.state_dict())
-            torch.save(best_state_dict, model_path)
-            prev_test_acc = robust_acc
+        losses_arr, correct_arr, certificates_arr = evaluate_certificates(test_loader, model, L)
+        
+        test_loss, test_acc, test_cert, test_robust_acc_list = robust_statistics(
+            losses_arr, correct_arr, certificates_arr)
+        
+        robust_acc = test_robust_acc_list[0]
+        if (robust_acc > prev_robust_acc):
+            torch.save(model.state_dict(), best_model_path)
+            prev_robust_acc = robust_acc
             best_epoch = epoch
         
         epoch_time = time.time()
         lr = scheduler.get_last_lr()[0]
         logger.info('%d \t %.1f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f',
             epoch, epoch_time - start_epoch_time, lr, train_loss/train_n, train_acc/train_n, 
-            train_robust/train_n, train_cert/train_acc, test_loss, test_acc, robust_acc, mean_cert)
+            test_loss, test_acc, test_robust_acc_list[0], test_robust_acc_list[1], 
+            test_robust_acc_list[2], test_cert)
         
-        model_path = os.path.join(args.out_dir, 'last.pth')
-        torch.save(last_state_dict, model_path)
+        torch.save(model.state_dict(), last_model_path)
         
         trainer_state_dict = { 'epoch': epoch, 'optimizer_state_dict': opt.state_dict()}
-        opt_path = os.path.join(args.out_dir, 'last_opt.pth')
-        torch.save(trainer_state_dict, opt_path)
-
-    if args.conv_type=='skew':
-        sigma_array = test_real_sn(model)
-        s_min, s_mean, s_max = sigma_array.min(), sigma_array.mean(), sigma_array.max()
-        logger.info('Real sigma statistics: %.4f \t %.4f \t %.4f', s_min, s_mean, s_max)            
+        torch.save(trainer_state_dict, last_opt_path)
         
     train_time = time.time()
 
     logger.info('Total train time: %.4f minutes', (train_time - start_train_time)/60)
     
-    start_test_time = time.time()
-    # Evaluation at early stopping
-    model_test = LipNet_n(args.conv_type, in_channels=args.in_channels, 
-                     num_blocks = args.block_size, num_classes=num_classes).cuda()
-    model_test.load_state_dict(best_state_dict)
+    
+    # Evaluation at best model (early stopping)
+    model_test = init_model(args).cuda()
+    model_test.load_state_dict(torch.load(best_model_path))
     model_test.float()
     model_test.eval()
         
     start_test_time = time.time()
-    test_loss, test_acc, mean_cert, robust_acc = evaluate_certificates(test_loader, model_test, L)
+    losses_arr, correct_arr, certificates_arr = evaluate_certificates(test_loader, model_test, L)
     total_time = time.time() - start_test_time
     
-    logger.info('Best Epoch \t Test Loss \t Test Acc \t Robust Acc \t Mean Cert \t Test Time')
-    logger.info('%d \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f', best_epoch, test_loss, 
-                                                      test_acc, robust_acc, 
-                                                      mean_cert, total_time)
+    test_loss, test_acc, test_cert, test_robust_acc_list = robust_statistics(
+        losses_arr, correct_arr, certificates_arr)
+    
+    logger.info('Best Epoch \t Test Loss \t Test Acc \t Test Robust (36) \t Test Robust (72) \t Test Robust (108) \t Mean Cert \t Test Time')
+    logger.info('%d \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f', best_epoch, test_loss, test_acc,
+                                                        test_robust_acc_list[0], test_robust_acc_list[1], 
+                                                        test_robust_acc_list[2], test_cert, total_time)
 
     # Evaluation at last model
-    model_test.load_state_dict(last_state_dict)
+    model_test.load_state_dict(torch.load(last_model_path))
     model_test.float()
     model_test.eval()
 
     start_test_time = time.time()
-    test_loss, test_acc, mean_cert, robust_acc = evaluate_certificates(test_loader, model_test, L)
+    losses_arr, correct_arr, certificates_arr = evaluate_certificates(test_loader, model_test, L)
     total_time = time.time() - start_test_time
     
-    logger.info('Last Epoch \t Test Loss \t Test Acc \t Robust Acc \t Mean Cert \t Test Time')
-    logger.info('%d \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f', epoch, test_loss, 
-                                                        test_acc, robust_acc, 
-                                                        mean_cert, total_time)
+    test_loss, test_acc, test_cert, test_robust_acc_list = robust_statistics(
+        losses_arr, correct_arr, certificates_arr)
+    
+    logger.info('Last Epoch \t Test Loss \t Test Acc \t Test Robust (36) \t Test Robust (72) \t Test Robust (108) \t Mean Cert \t Test Time')
+    logger.info('%d \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f', epoch, test_loss, test_acc,
+                                                        test_robust_acc_list[0], test_robust_acc_list[1], 
+                                                        test_robust_acc_list[2], test_cert, total_time)
 
 if __name__ == "__main__":
     main()
